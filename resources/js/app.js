5bdf9b741ad8b69ff91195c35fa7d86a1aad3921
Matthew Chick <c.chick@unsw.edu.au>
2020-12-30 22:21:40 +1100

    Add vue router and using <router-view> to show routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,19 @@
 
 require('./bootstrap');
 
+import router from "./routes";
+import VueRouter from "vue-router";
+
 window.Vue = require('vue');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    'example-component',
+    require('./components/ExampleComponent.vue').default
+);
+
+Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
+    router,
 });
