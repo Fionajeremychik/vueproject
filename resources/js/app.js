b1076187133a128024f6f39b07b86e7527ef6a13
Matthew Chick <c.chick@unsw.edu.au>
2021-01-01 20:58:47 +1100

    how to use index component

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,21 +8,25 @@ require('./bootstrap');
 
 import router from "./routes";
 import VueRouter from "vue-router";
+import Index from "./index.vue";
 
 window.Vue = require('vue');
 
-Vue.component(
+/* Vue.component(   // Use index.vue
     'example-component',
     require('./components/ExampleComponent.vue').default
 );
 Vue.component(
     'Example2',
     require('./components/Example2.vue').default
-);
+); */
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
     router,
+    components: {
+        "index": Index
+    }
 });
