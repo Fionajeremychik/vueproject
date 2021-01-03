3f6a801cebd8b73f4ac4fcb6bbefe0c0fa1fabd6
Matthew Chick <c.chick@unsw.edu.au>
2021-01-03 21:21:21 +1100

    how to create and register a child component

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <item></item>
+    </div>
+</template>
+
+<script>
+import BookableListitem from "./BookableListitem";
+
+export default {
+    components: {
+        item: BookableListitem
+    }
+}
+</script>
